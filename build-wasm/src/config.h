6cf71d15b8368cda4e816b3964d128d30713d043
Gvbvdxx (Alt Account) <jgevans@students.wcpss.net>
2025-12-16 16:53:18 +0000

    Commit

--- build-wasm/src/config.h
+++ build-wasm/src/config.h
@@ -11,7 +11,7 @@
 
 #ifdef CMAKECONFIG
 
-#define SRB2_COMP_REVISION       "6996f2bd6"
+#define SRB2_COMP_REVISION       "84753924a"
 #define SRB2_COMP_BRANCH         "master"
 #define SRB2_COMP_NOTE           "Commit"
 // This is done with configure_file instead of defines in order to avoid
