57e28aeb6adc1b3435fa3282959ce446743ca31b
Gvbvdxx (Alt Account) <jgevans@students.wcpss.net>
2025-12-16 13:26:40 +0000

    Commit

--- build-wasm.sh
+++ build-wasm.sh
@@ -4,14 +4,16 @@
 mkdir build-wasm
 cd build-wasm
 
-emcmake cmake .. \
-    -DCMAKE_TOOLCHAIN_FILE=../libs/vcpkg/scripts/buildsystems/vcpkg.cmake \
-    -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
-    -DVCPKG_TARGET_TRIPLET=wasm32-emscripten \
-    -DSRB2_CONFIG_ENABLE_DISCORDRPC=OFF \
-    -DSRB2_CONFIG_ENABLE_WEBM_MOVIES=OFF \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DS_USE_SYSTEM_SDL2=ON \
-    -DCMAKE_EXE_LINKER_FLAGS="-s USE_SDL=2 -s USE_SDL_MIXER=2 -s ASYNCIFY"
+emcmake cmake .. -DCMAKE_BUILD_TYPE=Release \
+    -DUSE_GME=0 -DUSE_OPENMPT=0 -DUSE_UPNP=0 -DUSE_SDL2_NET=0 -DUSE_PHYSFS=0 -DUSE_ZLIB=1 -DUSE_LIBPNG=1 -DUSE_CURL=0 \
+    '-DCMAKE_C_FLAGS=-s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_MIXER=2 -s USE_SDL_TTF=2 -s USE_ZLIB=1 -s USE_LIBPNG=1 -s ASYNCIFY=1' \
+    '-DCMAKE_EXE_LINKER_FLAGS=-s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_MIXER=2 -s USE_SDL_TTF=2 -s USE_ZLIB=1 -s USE_LIBPNG=1 -s ASYNCIFY=1 -s ALLOW_MEMORY_GROWTH=1 -s EXPORTED_RUNTIME_METHODS=["ccall","cwrap","UTF8ToString","stringToUTF8","lengthBytesUTF8","allocate","intArrayFromString"]' \
+    -DZLIB_FOUND=TRUE -DZLIB_INCLUDE_DIR=/workspaces/SRB2web/emsdk/upstream/emscripten/cache/sysroot/include -DZLIB_LIBRARY=zlib \
+    -DPNG_FOUND=TRUE -DPNG_PNG_INCLUDE_DIR=/workspaces/SRB2web/emsdk/upstream/emscripten/cache/sysroot/include -DPNG_LIBRARY=png \
+    -DCURL_FOUND=TRUE -DCURL_INCLUDE_DIR=/workspaces/SRB2/libs/curl/include -DCURL_LIBRARY=curl \
+    -DSDL2_FOUND=TRUE -DSDL2_LIBRARIES=SDL2 -DSDL2_DIR=/workspaces/SRB2web/emsdk/upstream/emscripten/cache/sysroot/lib/cmake/SDL2 \
+    -DSDL2_CONFIG_INCLUDE_DIR=/workspaces/SRB2web/emsdk/upstream/emscripten/cache/sysroot/include \
+    -DCMAKE_TOOLCHAIN_FILE=/workspaces/SRB2web/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
+    -DCMAKE_CROSSCOMPILING_EMULATOR=/workspaces/SRB2web/emsdk/node/22.16.0_64bit/bin/node
 
 emmake make -j$(nproc)
---
+++ build-wasm/src/CMakeFiles/_SRB2_reconf.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_SRB2_reconf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/_SRB2_reconf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
