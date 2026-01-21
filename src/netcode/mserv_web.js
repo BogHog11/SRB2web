ee989746edfcf8d247d364b2dee9607a7dbfee94
gvbvdxx <123641869+gvbvdxx@users.noreply.github.com>
2026-01-21 14:48:12 +0000

    Commit

--- src/netcode/mserv_web.js
+++ src/netcode/mserv_web.js
@@ -5,12 +5,12 @@ mergeInto(LibraryManager.library, {
     }
     return 1;
   },
-  SRB2_ServerInfoResponse: function (serverName, map) {
+  SRB2_ServerInfoResponse: function (serverName, map, mapTitle) {
     if (window.SRB2_ServerInfoResponse) {
       return window.SRB2_ServerInfoResponse({
         name: UTF8ToString(serverName),
         map: UTF8ToString(map),
-        mapName: UTF8ToString(mapName),
+        mapTitle: UTF8ToString(mapTitle),
       });
     }
     return 1;
