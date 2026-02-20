279f47e85e997bb027de454c5b31aa382bdbfd2c
Gvbvdxx (Alt Account) <gvbvdxxalt2@outlook.com>
2026-02-20 12:48:32 -0500

    Update load-emsdk.bat

--- load-emsdk.bat
+++ load-emsdk.bat
@@ -1,22 +1,19 @@
 @echo off
-REM Load Emscripten SDK on Windows
-
-if exist emsdk (
-    echo emsdk already exists, activating latest.
+if exist "emsdk\" (
+    echo emsdk already exists, skipping clone.
     cd emsdk
-    emsdk activate latest
+    call emsdk activate latest
     cd ..
 ) else (
     git clone https://github.com/emscripten-core/emsdk.git
     cd emsdk
-    emsdk install latest
-    emsdk activate latest
+    call emsdk install latest
+    call emsdk activate latest
     cd ..
 )
 
-REM Source the environment (for Windows, assume emsdk_env.bat is called)
-if exist emsdk\emsdk_env.bat (
-    call emsdk\emsdk_env.bat
+if exist "emsdk\emsdk_env.bat" (
+    call .\emsdk\emsdk_env.bat
 ) else (
     echo emsdk_env.bat not found. Please check emsdk installation.
     exit /b 1
