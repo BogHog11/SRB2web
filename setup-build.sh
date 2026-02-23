83c9303c31b814bbb49883d87e7565329058f831
Gvbvdxx (Alt Account) <gvbvdxxalt2@outlook.com>
2026-02-23 10:17:47 -0500

    Update setup-build.sh

--- setup-build.sh
+++ setup-build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+set -e
 
-if [ -d "libs/vcpkg" ]; then
-  echo "libs/vcpkg is already setup"
+VCPKG_PATH="libs/vcpkg"
+
+# Check if the vcpkg directory exists and actually has a .git folder
+if [ -d "$VCPKG_PATH/.git" ]; then
+    echo "libs/vcpkg is already setup."
 else
-  cd libs
-  git clone https://github.com/microsoft/vcpkg
-  cd ../
+    echo "Cloning vcpkg into $VCPKG_PATH..."
+    # No need to cd; git clone can take a target directory
+    git clone https://github.com/microsoft/vcpkg "$VCPKG_PATH"
 fi
+
+# Ensure scripts are executable
+chmod +x ./vcpkg-setup.sh ./build-wasm.sh
 
 source ./load-emsdk.sh
 ./vcpkg-setup.sh
-
 ./build-wasm.sh
